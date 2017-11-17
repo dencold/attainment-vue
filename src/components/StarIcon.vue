0c464a40c0918fbf14f95ae9110784991e9ad4b2
Dennis Coldwell <dennis.coldwell@gmail.com>
2017-11-16 16:48:09 -0800

    adding staricon

---
+++ src/components/StarIcon.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-icon class="icon" @click="toggleStar">{{iconType}}</v-icon>
+</template>
+
+<script>
+  export default {
+
+    data() {
+      return {
+        starred: false,
+      };
+    },
+
+    computed: {
+      iconType() {
+        if (this.starred) {
+          return 'star';
+        }
+
+        return 'star_border';
+      },
+    },
+
+    methods: {
+      toggleStar() {
+        this.starred = !this.starred;
+        this.$emit('star-toggled', { starred: this.starred });
+      },
+    },
+
+  };
+</script>
+
+<style scoped>
+  .icon {
+    cursor: pointer;
+  }
+</style>
